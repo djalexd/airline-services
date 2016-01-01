6d1a5b0b2073da5a615c21c9508db01af9bf6032
Alex Dobjanschi <alex.dobjanschi@gmail.com>
2016-01-01 23:16:59 +0200

    spring boot start app

---
+++ src/main/java/com/github/acme/airlines/AirlinesServicesApplication.java
@@ -0,0 +1,12 @@
+package com.github.acme.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesServicesApplication.class, args);
+	}
+}
